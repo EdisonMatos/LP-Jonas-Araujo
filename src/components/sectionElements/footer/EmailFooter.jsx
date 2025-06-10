1c88fd0fba9376acb981783f0948f7770d49de38
Edison Matos <edison.matos@live.com>
2025-06-10 09:56:47 -0300

    Initial commit

---
+++ src/components/sectionElements/footer/EmailFooter.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Mail } from "lucide-react";
+import MotionDivDownToUp from "../../animation/MotionDivDownToUp";
+import content from "../../../content/content";
+
+function EmailFooter() {
+  return (
+    <MotionDivDownToUp>
+      <div className="flex full gap-x-[12px] items-center opacity-90">
+        <Mail />
+
+        {/* <p className="text-paragraph2">{content.texts.infos.email}</p> */}
+        <p className="text-paragraph2">joaomarcelo@hamuopa.adv.br</p>
+      </div>
+    </MotionDivDownToUp>
+  );
+}
+
+export default EmailFooter;
