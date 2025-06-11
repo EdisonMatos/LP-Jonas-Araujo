2aea2b3744b8a90b70de881389823af4305b8ec0
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-06-11 08:24:51 -0300

    fix: Tamanho da logo

--- src/components/sectionElements/Lp01/hero/FullImg.jsx
+++ src/components/sectionElements/Lp01/hero/FullImg.jsx
@@ -30,7 +30,7 @@ function FullImg({ obs }) {
               <img
                 src={contentLp01.hero.logo.logoImg}
                 alt={contentLp01.hero.logo.alt}
-                className="ml-[5%] max-w-[50%] mt-[15%] p-[5px] bg-white rounded-lg"
+                className="ml-[5%] max-w-[30%] mt-[15%] p-[10px] bg-white rounded-lg"
               />
             </MotionDivDownToUp>
 
